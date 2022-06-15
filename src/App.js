b75449707fb2085fc9fbc2cd416f9845ff2d4a58
Kelly Ma <kellyma@wustl.edu>
2022-06-15 16:56:44 -0500

    Renders input image

--- src/App.js
+++ src/App.js
@@ -1,35 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { render } from '@testing-library/react';
 
 class Header extends React.Component {
   render() {
     return (
       <p>web photo editor</p>
     );
-    
   }
 }
 
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.browseFiles = this.browseFiles.bind(this);
+    this.displayImage = this.displayImage.bind(this);
+    this.state = {
+      imgURL: ""
+    };
+  }
+  
+  displayImage(event) {
+    document.getElementsByClassName("drop")[0].style.display = "none";
+    const fileInput = document.getElementById("browse-files");
+    const uploadedFiles = fileInput.files;
+  
+    if (uploadedFiles.length === 1) {
+      this.setState( {imgURL : URL.createObjectURL(uploadedFiles[0])} );
+      // this.setStateimgURL = URL.createObjectURL(uploadedFiles[0]);
+      console.log(this.state.imgURL);
+      console.log(uploadedFiles[0].name);
+      console.log(uploadedFiles[0].size); 
+    }
+  }
+
+  browseFiles(event) {
+    event.preventDefault();
+    document.getElementById("browse-files").click();
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <div className="drop">
+          <p>Drag and drop a file to edit</p>
+          <input type="file" id="browse-files" name="file" accept=".jpg,.jpeg,.png,.raw,.cr2,.nef,.bmp,.tif,.tiff" onChange={this.displayImage}/>
+          <button id="custom-upload" onClick={this.browseFiles}>Browse</button>
+        </div>
+        {<UploadedImage texts="hihihi" imageURL={this.state.imgURL}/>}
+      </div>
+    );
+  }
+}
+
+class UploadedImage extends React.Component {
+  render() {
+    console.log("show uploaded image");
+    console.log(this.props.imageURL);
+    return (
+      <div>
+        { (this.props.imageURL !== "") && <img src={this.props.imageURL} alt="uploaded image" />}
+      </div>
+    );
+  }
+}
+
+
 function App() {
+
+
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
-        {<Header />
-        /* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */
-        }
+        <Header />
       </header>
+      
+      <Content />
     </div>
   );
 }
