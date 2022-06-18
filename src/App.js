d1d4cef7c33e3cd31779e5144257dcf604831e79
Kelly Ma <kellyma@wustl.edu>
2022-06-18 17:51:51 -0500

    invert, revert, change image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import React from 'react';
-// import Slider from '@mui/material/Slider';
-// import Typography from '@mui/material/Typography';
-import Editor from './Editor.js';
-
+import Content from './Content.js';
 
 class Header extends React.Component {
   render() {
@@ -12,111 +9,6 @@ class Header extends React.Component {
     );
   }
 }
-
-class Content extends React.Component {
-  constructor(props) {
-    super(props);
-    this.browseFiles = this.browseFiles.bind(this);
-    this.uploadImage = this.uploadImage.bind(this);
-    this.displayImage = this.displayImage.bind(this);
-    this.dropHandler = this.dropHandler.bind(this);
-    this.dragOverHandler = this.dragOverHandler.bind(this);
-    this.handleImageLoaded = this.handleImageLoaded.bind(this);
-
-    this.state = {
-      imgURL: "",
-      originalImageData: null,
-      newImageData: null
-    };
-  }
-  
-  uploadImage(event) {
-    const fileInput = document.getElementById("browse-files");
-    const uploadedFiles = fileInput.files;
-    if (uploadedFiles.length > 0) {
-      const uploadedFile = uploadedFiles[0];
-      this.displayImage(uploadedFile);
-    }
-  }
-
-  displayImage(file) {
-    console.log(file);
-    const imageTypes = ['image/jpg','image/jpeg','image/png','image/raw','image/cr2','image/nef','image/bmp','image/tif','image/tiff'];
-    if (file && imageTypes.includes(file['type'])) {
-      document.getElementsByClassName("drop")[0].style.display = "none";
-      this.setState( {imgURL : URL.createObjectURL(file)} ); // Setting state triggers re-render
-    }
-  }
-
-  // Recognize drop event on target element
-  dropHandler(event) {
-    console.log("Drop handler");
-    event.preventDefault();
-    let file;
-    if (event.dataTransfer.items) {
-      if (event.dataTransfer.items[0].kind === 'file') {
-        file = event.dataTransfer.items[0].getAsFile();
-        console.log("file name: " + file.name);
-      }
-      else {
-        file = event.dataTransfer.files[0];
-        console.log("name: " + file.name);
-      }
-      this.displayImage(file);
-    }
-  }
-
-  // Turns off browser's default drag behavior over the target element
-  dragOverHandler(event) {
-    console.log("Dragover handler");
-    event.preventDefault();
-  }
-
-  browseFiles(event) {
-    event.preventDefault();
-    document.getElementById("browse-files").click();
-  }
-
-  handleImageLoaded(event) {
-    let myImg = document.getElementById("image-upload");
-    let imgCanvas = document.getElementById("image-canvas");
-    imgCanvas.width = myImg.naturalWidth;
-    imgCanvas.height = myImg.naturalHeight;
-    imgCanvas.naturalWidth = myImg.naturalWidth;
-    imgCanvas.naturalHeight = myImg.naturalHeight;
-
-    let ctx = imgCanvas.getContext("2d");
-    ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(myImg, 0, 0, myImg.naturalWidth, myImg.naturalHeight);
-    let imgData = ctx.getImageData(0, 0, imgCanvas.width, imgCanvas.height)
-    let newData = new Uint8ClampedArray(imgData.data);
-    let newImgData = new ImageData(newData, imgData.width, imgData.height, { colorspace: "srgb" });
-    this.setState({ originalImageData: imgData, newImageData: newImgData }); // Trigger re-render of UploadedImage to view newly drawn image and display editor
-  }
-
-  render() {
-    return (
-      <div className="content">
-        <div className="drop" onDrop={this.dropHandler} onDragOver={this.dragOverHandler}>
-          <p>drag and drop a file to edit</p>
-          <input type="file" id="browse-files" name="file" accept=".jpg,.jpeg,.png,.raw,.cr2,.nef,.bmp,.tif,.tiff" onChange={this.uploadImage}/>
-          <button id="custom-upload" onClick={this.browseFiles}>browse</button>
-        </div>
-        { (this.state.imgURL !== "") && 
-          <div id="previews">
-            <img id="image-upload" src={this.state.imgURL} alt="original upload" onLoad={this.handleImageLoaded}/>
-            <canvas id="image-canvas" alt="edited upload"></canvas>
-          </div>
-        }
-        { (this.state.originalImageData) && (this.state.newImageData) && <Editor originalImageData={this.state.originalImageData} newImageData={this.state.newImageData}/>}
-      </div>
-    );
-  }
-}
-
-
-
-
 
 function App() {
   return (
--- src/Editor.js
+++ src/Editor.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
@@ -24,12 +23,17 @@ const theme = createTheme({
 export default class Editor extends React.Component {
     constructor(props) {
       super(props);
-      this.doEdit = this.doEdit.bind(this);
+			this.revert = this.revert.bind(this);
+      this.grayscale = this.grayscale.bind(this);
+			this.invert = this.invert.bind(this);
+			// this.updateOriginal = this.updateOriginal.bind(this);
       this.filterBrightness = this.filterBrightness.bind(this);
+			this.filterContrast = this.filterContrast.bind(this);
       this.handleBrightness = this.handleBrightness.bind(this);
       this.handleContrast = this.handleContrast.bind(this);
       this.handleDownload = this.handleDownload.bind(this);
 			this.runFilters = this.runFilters.bind(this);
+			this.handleUpload = this.handleUpload.bind(this);
       this.state = {
         originalData: this.props.originalImageData,
         newImageData: this.props.newImageData,
@@ -38,38 +42,72 @@ export default class Editor extends React.Component {
       }
     }
 
-    doEdit() {
-      let myImg = document.getElementById("image-upload");
-      let imgCanvas = document.getElementById("image-canvas");
-      if (myImg.naturalWidth === imgCanvas.width) { // Ensure canvas image has been drawn first
-        let ctx = imgCanvas.getContext('2d');
-        const imageData = ctx.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
-        const data = imageData.data;
-        for (let i=0; i<data.length; i += 4) {
-          const avg = (data[i] + data[i+1] + data[i+2]) / 3;
-          data[i] = avg;
-          data[i+1] = avg;
-          data[i+2] = avg;
-        }
-        ctx.putImageData(imageData, 0, 0);
-      }
+		componentDidUpdate(prevProps) {
+			if (prevProps.originalImageData !== this.props.originalImageData) {
+				this.setState({ originalData: this.props.originalImageData,
+					newImageData: this.props.newImageData,
+					brightnessValue: 0,
+					contrastValue: 0
+				});
+			}
+		}
+
+		// Put originalImageData on the canvas and reset values
+		revert() {
+			this.setState( { brightnessValue: 0 });
+			this.setState( { contrastValue: 0 });
+			this.runFilters(0, 0);
+		}
+
+		// updateOriginal() {
+
+		// }
+
+    grayscale() {
+      const imgCanvas = document.getElementById("image-canvas");
+			const ctx = imgCanvas.getContext('2d');
+			const imageData = ctx.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
+			const data = imageData.data;
+			for (let i=0; i<data.length; i += 4) {
+				const avg = (data[i] + data[i+1] + data[i+2]) / 3;
+				data[i] = avg;
+				data[i+1] = avg;
+				data[i+2] = avg;
+			}
+			ctx.putImageData(imageData, 0, 0);
+			// updateOriginal();
     }
+
+		invert() {
+      const imgCanvas = document.getElementById("image-canvas");
+			const ctx = imgCanvas.getContext('2d');
+			const imageData = ctx.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
+			const data = imageData.data;
+			for (let i=0; i<data.length; i += 4) {
+				data[i] = 255 - data[i];
+				data[i+1] = 255 - data[i+1];
+				data[i+2] = 255 - data[i+2];
+			}
+			ctx.putImageData(imageData, 0, 0);
+			// updateOriginal();
+		}
   
+		// Store filtered originalImageData in newImageData
     filterBrightness(value) {
       console.log("filterBrightness")
-      const data = this.state.originalData.data;
-      const newImgData = this.state.newImageData;
-      const newData = newImgData.data;
-      for (let i=0; i<data.length; i += 4) {
-        newData[i] = Math.max(0, Math.min(255, data[i] - Math.round(255 * -(value/100)))); // r
-        newData[i+1] = Math.max(0, Math.min(255, data[i+1] - Math.round(255 * -(value/100)))); // g
-        newData[i+2] = Math.max(0, Math.min(255, data[i+2] - Math.round(255 * -(value/100)))); // b
-        newData[i+3] = data[i+3]; // a
+      const origData = this.state.originalData.data;
+      const imageData = this.state.newImageData;
+      const data = imageData.data;
+      for (let i=0; i<origData.length; i += 4) {
+        data[i] = Math.max(0, Math.min(255, origData[i] - Math.round(255 * -(value/100)))); // r
+        data[i+1] = Math.max(0, Math.min(255, origData[i+1] - Math.round(255 * -(value/100)))); // g
+        data[i+2] = Math.max(0, Math.min(255, origData[i+2] - Math.round(255 * -(value/100)))); // b
       }
       let ctx = document.getElementById("image-canvas").getContext('2d');
-      ctx.putImageData(newImgData, 0, 0);
+      ctx.putImageData(imageData, 0, 0);
     }
   
+		// Replace newImageData with filtered newImageData
     filterContrast(value) {
       console.log("filterContrast")
       value *= 2.55;
@@ -107,14 +145,19 @@ export default class Editor extends React.Component {
 		}
 
     handleDownload(event) {
-			const imageData = document.getElementById("image-canvas").toDataURL();
+			const imageData = document.getElementById("image-canvas").toDataURL('image/jpeg', 0.8);
 			let tempLink = document.createElement('a');
-			tempLink.download = 'edited_image.png';
+			tempLink.download = 'edited.jpeg';
 			tempLink.href = imageData;
 			document.body.appendChild(tempLink);
 			tempLink.click();
 			document.body.removeChild(tempLink);
     }
+
+		handleUpload(event) {
+			console.log("handleUpload");
+			document.getElementById("browse-files").click();
+		}
   
     // Edit options: grayscale, saturation, brightness, contrast
     // Provide slider a pre-slider edit ctx relative to which brightness is modified by the slider
@@ -123,9 +166,11 @@ export default class Editor extends React.Component {
 	
       return (
         <div id="editor"> 
-          <button id="editor-upload" >Upload</button>
+					<button id="editor-revert" onClick={this.revert}>Revert changes</button>
+          <button id="editor-upload" onClick={this.handleUpload}>Upload</button>
           <button id="editor-download" onClick={this.handleDownload}>Download</button>
-          <button id="edit-photo" onClick={this.doEdit}>Grayscale</button>
+          <button id="grayscale-btn" onClick={this.grayscale}>Grayscale</button>
+					<button id="invert-btn" onClick={this.invert}>Invert</button>
           <ThemeProvider theme={theme}>
             <Typography id="brightness-label" color="primary.contrastText" >Brightness</Typography>
             <Slider 
@@ -134,8 +179,8 @@ export default class Editor extends React.Component {
               value={this.state.brightnessValue} 
               min={0} 
               max={100} 
-              step={20} 
-              marks 
+              // step={20} 
+              // marks 
               valueLabelDisplay="auto" 
               onChangeCommitted={this.handleBrightness}
             />
@@ -146,8 +191,8 @@ export default class Editor extends React.Component {
               value={this.state.contrastValue} 
               min={-100} 
               max={100} 
-              step={20} 
-              marks 
+              // step={20} 
+              // marks 
               valueLabelDisplay="auto" 
               onChangeCommitted={this.handleContrast}
             />
